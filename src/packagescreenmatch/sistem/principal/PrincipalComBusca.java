c97ea5bd828dc7fc318fb6330d2d0472493ee301
Tallis Teixeira <tallisteixeiradev@gmail.com>
2024-12-04 10:29:05 -0300

    Colocando metodo de busca de filme api

--- src/packagescreenmatch/sistem/principal/PrincipalComBusca.java
+++ src/packagescreenmatch/sistem/principal/PrincipalComBusca.java
@@ -5,12 +5,18 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um filme para buscar: ");
+        var busca = sc.nextLine();
+        // https://www.omdbapi.com/?t=avengers&apikey=72019bad
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=72019bad";
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?i=tt3896198&apikey=31e3efd9"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
