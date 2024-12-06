2ee9a5171c3ac8279a403e50856803dae54731b3
Tallis Teixeira <tallisteixeiradev@gmail.com>
2024-12-06 04:47:21 -0300

    Mexendo com Bibliotecas

--- src/packagescreenmatch/sistem/principal/PrincipalComBusca.java
+++ src/packagescreenmatch/sistem/principal/PrincipalComBusca.java
@@ -1,5 +1,8 @@
 package packagescreenmatch.sistem.principal;
 
+import com.google.gson.Gson;
+import packagescreenmatch.sistem.modelos.Titulo;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -10,16 +13,26 @@ import java.util.Scanner;
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite um filme para buscar: ");
+        System.out.print("Digite um filme para buscar: ");
         var busca = sc.nextLine();
         // https://www.omdbapi.com/?t=avengers&apikey=72019bad
         String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=72019bad";
+
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+        String json = response.body();
+
+        System.out.println(json);
+
+        Gson gson = new Gson();
+        Titulo primeiroTitulo = gson.fromJson(json, Titulo.class);
+        System.out.println(primeiroTitulo);
+
+
     }
 }
